c8e49edf14ec40db07b5c6355efbdac7cd985dc8
Adam Simmons <adam@ultralig.ht>
2025-07-01 12:22:56 -0500

    Fix issue on Windows where idle run loop wouldn't wake on external SendMessage() call (fixes ultralight-ux/Ultralight#531).

--- src/win/AppWin.cpp
+++ src/win/AppWin.cpp
@@ -200,7 +200,7 @@ void AppWin::Run()
 
     while (is_running_) {
         // Wait for the timer or a message, whichever comes first
-        DWORD result = MsgWaitForMultipleObjects(2, timers, FALSE, INFINITE, QS_ALLEVENTS);
+        DWORD result = MsgWaitForMultipleObjects(2, timers, FALSE, INFINITE, QS_ALLINPUT);
 
         if (result == WAIT_OBJECT_0) {
             Update();
